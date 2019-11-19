d4ddb1d0fa4e096b98b58a8b0e14c7cdc10e1452
Dani Fdez <danifdezalvarez@gmail.com>
2019-11-19 17:47:57 +0100

    Fix setpup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from distutils.core import setup
+from setuptools import find_packages
+
 setup(
   name = 'shexer',
-  packages = ['shexer'], # this must be the same as the name above
-  version = '0.0.3',
+  packages = find_packages(), # this must be the same as the name above
+  version = '0.0.4',
   description = 'Automatic schema extraction for RDF graphs',
   author = 'Daniel Fernandez-Alvarez',
   author_email = 'danifdezalvarez@gmail.com',
   url = 'https://github.com/DaniFdezAlvarez/shexerp3',
-  download_url = 'https://github.com/DaniFdezAlvarez/shexer/tarball/0.0.3',
+  download_url = 'https://github.com/DaniFdezAlvarez/shexer/tarball/0.0.4',
   keywords = ['testing', 'shexer', 'shexerp3', "rdf", "shex", "schema"],
   classifiers = [],
 )
