07ed1dc9d37e1439025c8106076572a98ed498dd
Dani Fdez <danifdezalvarez@gmail.com>
2020-04-04 16:40:11 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -4,12 +4,19 @@ from setuptools import find_packages
 setup(
   name = 'shexer',
   packages = find_packages(exclude=["*.local_code.*"]), # this must be the same as the name above
-  version = '0.0.5',
+  version = '0.0.6',
   description = 'Automatic schema extraction for RDF graphs',
   author = 'Daniel Fernandez-Alvarez',
   author_email = 'danifdezalvarez@gmail.com',
   url = 'https://github.com/DaniFdezAlvarez/shexerp3',
-  download_url = 'https://github.com/DaniFdezAlvarez/shexer/tarball/0.0.5',
+  download_url = 'https://github.com/DaniFdezAlvarez/shexer/tarball/0.0.6',
   keywords = ['testing', 'shexer', 'shexerp3', "rdf", "shex", "schema"],
   classifiers = [],
+  install_requires=[            
+          'Flask',
+          'Flask-Cors',
+		  'rdflib',
+		  'SPARQLWrapper',
+		  'rdflib-jsonld'
+      ],
 )
