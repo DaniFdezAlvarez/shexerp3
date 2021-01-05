7858c0305d5cc90066fdb92c184c79aea031ca7c
Dani Fdez <danifdezalvarez@gmail.com>
2021-01-05 19:43:03 +0100

    Working on #73

--- test/test_infer_numeric_types_for_untyped_literals.py
+++ test/test_infer_numeric_types_for_untyped_literals.py
@@ -9,10 +9,24 @@ _BASE_DIR = BASE_FILES + "untyped_numbers\\"
 
 class TestInferNumericTypesForUntypedLiterals(unittest.TestCase):
 
-    def test_some_format(self):
+    def test_untyped_int(self):
         shaper = Shaper(target_classes=["http://xmlns.com/foaf/0.1/Person",
                                         "http://xmlns.com/foaf/0.1/Document"],
                         graph_file_input=_BASE_DIR + "g1_untyped_age.nt",
+                        namespaces_dict=NAMESPACES_WITH_FOAF_AND_EX,
+                        all_classes_mode=False,
+                        infer_numeric_types_for_untyped_literals=True,
+                        input_format=NT,
+                        disable_comments=True)
+        str_result = shaper.shex_graph(string_output=True)
+        print(str_result)
+        self.assertTrue(file_vs_str_tunned_comparison(file_path=G1_ALL_CLASSES_NO_COMMENTS,
+                                                      str_target=str_result))
+
+    def test_untyped_int_and_float(self):
+        shaper = Shaper(target_classes=["http://xmlns.com/foaf/0.1/Person",
+                                        "http://xmlns.com/foaf/0.1/Document"],
+                        graph_file_input=_BASE_DIR + "g1_untyped_age.nt",  # TODO CHANGE THIS AND MORE
                         namespaces_dict=NAMESPACES_WITH_FOAF_AND_EX,
                         all_classes_mode=False,
                         infer_numeric_types_for_untyped_literals=True,
