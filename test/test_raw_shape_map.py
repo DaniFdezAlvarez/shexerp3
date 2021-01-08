f701e65012681890f0e91965c08139efd5ce74b0
Dani Fdez <danifdezalvarez@gmail.com>
2021-01-08 18:03:49 +0100

    Close #80

--- test/test_raw_shape_map.py
+++ test/test_raw_shape_map.py
@@ -23,6 +23,20 @@ class TestRawShapeMap(unittest.TestCase):
         self.assertTrue(file_vs_str_tunned_comparison(file_path=_BASE_DIR + "a_node.shex",
                                                       str_target=str_result))
 
+    def test_prefixed_node(self):
+        shape_map = "ex:Jimmy@<Person>"
+        shaper = Shaper(graph_file_input=G1,
+                        namespaces_dict=NAMESPACES_WITH_FOAF_AND_EX,
+                        all_classes_mode=False,
+                        input_format=TURTLE,
+                        disable_comments=True,
+                        shape_map_raw=shape_map
+                        )
+        str_result = shaper.shex_graph(string_output=True)
+        # print(str_result)
+        self.assertTrue(file_vs_str_tunned_comparison(file_path=_BASE_DIR + "a_node.shex",
+                                                      str_target=str_result))
+
     def test_focus(self):
         shape_map = "{FOCUS a foaf:Person}@<Person>"
         shaper = Shaper(graph_file_input=G1,
@@ -35,4 +49,45 @@ class TestRawShapeMap(unittest.TestCase):
         str_result = shaper.shex_graph(string_output=True)
         # print(str_result)
         self.assertTrue(file_vs_str_tunned_comparison(file_path=_BASE_DIR + "focus_nodes.shex",
+                                                      str_target=str_result))
+
+    def test_focus_wildcard(self):
+        shape_map = "{FOCUS foaf:name _}@<WithName>"
+        shaper = Shaper(graph_file_input=G1,
+                        namespaces_dict=NAMESPACES_WITH_FOAF_AND_EX,
+                        all_classes_mode=False,
+                        input_format=TURTLE,
+                        disable_comments=True,
+                        shape_map_raw=shape_map
+                        )
+        str_result = shaper.shex_graph(string_output=True)
+        self.assertTrue(file_vs_str_tunned_comparison(file_path=_BASE_DIR + "focus_and_wildcard.shex",
+                                                      str_target=str_result))
+
+    def test_sparql_selector(self):
+        shape_map = "SPARQL \"select ?p where { ?p a foaf:Person }\"@<Person>"
+        shaper = Shaper(graph_file_input=G1,
+                        namespaces_dict=NAMESPACES_WITH_FOAF_AND_EX,
+                        all_classes_mode=False,
+                        input_format=TURTLE,
+                        disable_comments=True,
+                        shape_map_raw=shape_map
+                        )
+        str_result = shaper.shex_graph(string_output=True)
+        # print(str_result)
+        self.assertTrue(file_vs_str_tunned_comparison(file_path=_BASE_DIR + "focus_nodes.shex",
+                                                      str_target=str_result))
+
+    def test_several_shapemap_items(self):
+        shape_map = "{FOCUS a foaf:Person}@<Person>\n{FOCUS a foaf:Document}@<Document>"
+        shaper = Shaper(graph_file_input=G1,
+                        namespaces_dict=NAMESPACES_WITH_FOAF_AND_EX,
+                        all_classes_mode=False,
+                        input_format=TURTLE,
+                        disable_comments=True,
+                        shape_map_raw=shape_map
+                        )
+        str_result = shaper.shex_graph(string_output=True)
+        # print(str_result)
+        self.assertTrue(file_vs_str_tunned_comparison(file_path=_BASE_DIR + "several_shm_items.shex",
                                                       str_target=str_result))
