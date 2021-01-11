e28418edbaa60181daa1dc7c126bc68108c15a85
Dani Fdez <danifdezalvarez@gmail.com>
2021-01-12 00:10:31 +0100

    Work on #2, #87 and #96. Need to finish tests

--- shexer/utils/factories/shape_serializer_factory.py
+++ shexer/utils/factories/shape_serializer_factory.py
@@ -5,7 +5,7 @@ from shexer.io.shex.formater.shex_serializer import ShexSerializer
 def get_shape_serializer(output_format, shapes_list, target_file=None, string_return=False, namespaces_dict=None,
                          tolerance_to_keep_similar_rules=0.15, keep_less_specific=True,
                          instantiation_property=None, discard_useless_constraints_with_positive_closure=True,
-                         all_compliant_mode=True, disable_comments=False):
+                         all_compliant_mode=True, disable_comments=False, disable_or_statements=True):
     if output_format == SHEX:
         return ShexSerializer(target_file=target_file,
                               shapes_list=shapes_list,
@@ -16,6 +16,7 @@ def get_shape_serializer(output_format, shapes_list, target_file=None, string_re
                               instantiation_property_str=instantiation_property,
                               discard_useless_positive_closures=discard_useless_constraints_with_positive_closure,
                               all_compliant_mode=all_compliant_mode,
-                              disable_comments=disable_comments)
+                              disable_comments=disable_comments,
+                              disable_or_statements=disable_or_statements)
     else:
         raise ValueError("Currently unsupported format: " + output_format)
---
+++ test/test_disable_or_statements.py
@@ -0,0 +1,23 @@
+import unittest
+from shexer.shaper import Shaper
+from test.const import BASE_FILES, NAMESPACES_WITH_FOAF_AND_EX
+from test.t_utils import file_vs_str_tunned_comparison
+
+from shexer.consts import TURTLE
+
+_BASE_DIR = BASE_FILES + "disable_or\\"
+
+class TestGraphFileInput(unittest.TestCase):
+
+    def test_some_format(self):
+        shaper = Shaper(graph_file_input=_BASE_DIR + "g3_or_example.ttl",
+                        namespaces_dict=NAMESPACES_WITH_FOAF_AND_EX,
+                        all_classes_mode=True,
+                        input_format=TURTLE,
+                        disable_comments=True,
+                        disable_or_statements=False)
+        str_result = shaper.shex_graph(string_output=True)
+        print(str_result)
+        self.assertTrue(file_vs_str_tunned_comparison(file_path=_BASE_DIR + "or_enabled.shex",
+                                                      str_target=str_result,
+                                                      or_shapes=True))
